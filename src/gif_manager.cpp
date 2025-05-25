0bf0cded25c8bdce6243beb6811a9015859f34ea
PandaPC <jose.fornes.jimenez@gmail.com>
2025-05-25 15:52:34 +0200

    He configurado bien el tema de las wifis y AP

--- src/gif_manager.cpp
+++ src/gif_manager.cpp
@@ -1,4 +1,5 @@
 #include "gif_manager.h"
+#include "ota_web.h" // Necesario para acceso a drawWiFiStatusIcon
 
 TFT_eSPI* GIFManager::_tft = nullptr;
 uint16_t GIFManager::_lineBuffer[256];
@@ -19,6 +20,7 @@ void GIFManager::play() {
   if (_gif->open((uint8_t*)_currentGIF->data, _currentGIF->size, draw)) {
     _tft->startWrite();
     while (_gif->playFrame(true, NULL)) {
+      drawWiFiStatusIcon(); // <-- Siempre dibuja el icono encima de cada frame
       yield();
     }
     _gif->close();
@@ -40,4 +42,10 @@ void GIFManager::draw(GIFDRAW *pDraw) {
     _tft->setAddrWindow(pDraw->iX, pDraw->iY + pDraw->y, width, 1);
     _tft->pushPixels(_lineBuffer, width);
   }
+
+  // ✅ Añade esto para que se sobrepinte tras cada línea
+  if (pDraw->y + pDraw->iY <= 10) {
+    drawWiFiStatusIcon();  // Dibuja el icono si la línea afecta a su zona
+  }
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,11 @@
 #include "gifs/inicio.h"
 #include "gifs/normal.h"
 #include "gifs/mareado.h"
-
+#include "gifs/corazones.h"
+#include <gifs/movimiento1.h>
 // Primero inicializa el TFT
 TFT_eSPI tft = TFT_eSPI();
-
+int num = 0;
 // Luego incluye el OTA (usa `tft`)
 #include "ota_web.h"
 
@@ -28,7 +29,9 @@ GIFEntry gifList[] = {
   { estrella, sizeof(estrella) },   // 1
   { normal, sizeof(normal) },       // 2
   { incio, sizeof(incio) },         // 3 -> inicio
-  { mareado, sizeof(mareado) }      // 4 -> mareado
+  { mareado, sizeof(mareado) },
+  { corazones, sizeof(corazones) },
+  { movimiento1 ,sizeof(movimiento1) }      // 4 -> mareado
 };
 
 // Índices legibles
@@ -36,8 +39,12 @@ enum GifIndex {
   GUINO = 0,
   MAREADO = 4,
   NORMAL = 2,
-  INICIO = 3
+  INICIO = 3,
+  CORAZONES = 5,
+  MOVIMIENTO1 = 6,
+  ESTRELLA = 1
 };
+int aleatorio[] = {0, 1, 5, 6};
 
 void setup() {
   Serial.begin(115200);
@@ -71,7 +78,6 @@ void loop() {
   static unsigned long lastCheck = 0;
   const unsigned long checkInterval = 300;  // ms
   const float threshold = 1.2;              // Umbral de sacudida en g
-
   if (millis() - lastCheck > checkInterval) {
     lastCheck = millis();
 
@@ -89,13 +95,13 @@ void loop() {
       gifManager.setGIF(gifList[MAREADO]);
       gifManager.play();
     } else {
-      int num = random(0, 4);
+
+      num = random(0, 5);
+      
       gifManager.setGIF(gifList[NORMAL]);
       gifManager.play();
-      if(num >= 0 && num <= 1){
-        gifManager.setGIF(gifList[num]);
-        gifManager.play();
-      }
+      gifManager.setGIF(gifList[aleatorio[num]]);
+      gifManager.play();
     }
   }
 
